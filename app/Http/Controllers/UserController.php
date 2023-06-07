2d5abedd0ae4f1d98f5a77840edddd47d5f2ac49
ICTDeveloperD1S <desarrolladortic@delthac1.com>
2023-06-07 16:46:04 -0500

    USER: CRUD complete

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        
+        $requestData = $request->all();
+        $requestData['password'] = md5($request->input('password'));
+        
+        $user = User::create($requestData);
+        return response()->json($user, 201);
+    }
+
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
